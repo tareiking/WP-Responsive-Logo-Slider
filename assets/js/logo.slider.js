d98877a6a5011ffae2ce9f54d6d2e94497662183
Tarei King <tareiking@gmail.com>
2014-06-17 15:46:50 +1000

    Updated slick.js variables

--- assets/js/logo.slider.js
+++ assets/js/logo.slider.js
@@ -1,28 +1,31 @@
 (function($) {
 	$(document).ready(function() {
 	    $('.responsive').slick({
-	        dots: true,
-	        infinite: false,
-	        speed: 300,
+	        dots: false,
+	        arrows: true,
+	        infinite: true,
+	        autoplay: true,
+	        speed: 200,
 	        slidesToShow: 3,
-	        slidesToScroll: 3,
+	        slidesToScroll: 1,
 	        responsive: [{
 	            breakpoint: 1024,
 	            settings: {
+	                arrows: true,
 	                slidesToShow: 3,
 	                slidesToScroll: 3,
-	                infinite: true,
-	                dots: true
 	            }
 	        }, {
 	            breakpoint: 600,
 	            settings: {
 	                slidesToShow: 2,
+	                arrows: true,
 	                slidesToScroll: 2
 	            }
 	        }, {
 	            breakpoint: 480,
 	            settings: {
+	                arrows: true,
 	                slidesToShow: 1,
 	                slidesToScroll: 1
 	            }
